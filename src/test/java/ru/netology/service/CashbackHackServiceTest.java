2ffd2ff0023222a7d22fdd31eae38bc3f9f3b8d8
Olesya Sumatokhina <olesya_po@mail.ru>
2024-02-05 14:04:51 +0300

    changeTestJunit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(900);
         int expected = 100;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
@@ -21,15 +21,8 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(1100);
         int expected = 900;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
-    //   @Test
-    //  public void cashNotTaskAfterSpendTest() {
-    //  CashbackHackService service = new CashbackHackService();
 
-    //  int actual = service.remain(1000);
-//      int expected = 0;
-    //  Assert.assertEquals(actual, expected);
-    //  }
 }
